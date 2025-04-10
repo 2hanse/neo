dc535d278d59e77c5a95c15aed37391d184b0eca
2hanse <prsuw113@naver.com>
2025-04-10 17:04:46 +0900

    python_0410

---
+++ python/pythonData/p83_lamdaExam.py
@@ -0,0 +1,9 @@
+def nolamda(x, y):
+    return 3 * x + 2 * y
+
+x, y = 3, 5
+
+yeslambda = lambda x, y: 3 * x + 2 * y
+
+print(f'nolambda({x}, {y} = {nolamda(x, y)}')
+print(f'yeslambda({x}, {y} = {yeslambda(x, y)}')
