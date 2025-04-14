bfeed20dc10923b1a2de1f85742e9aee4f9f97a4
2hanse <prsuw113@naver.com>
2025-04-14 13:59:12 +0900

    fix & matplotlib_png

--- python/pythonData/Ex_p239_07.py
+++ python/pythonData/Ex_p239_07.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 plt.rcParams['font.family'] = 'NanumBarunGothic'
 
-filename = 'mygraph.csv'
+fliename = 'mygraph.csv'
 
-myframe = pd.read_csv(filename, index_col='이름', encoding='utf-8')
+myframe = pd.read_csv(fliename, index_col='이름', encoding='utf-8')
 myframe.index.name = '이름'
 myframe.columns.name = '시험 과목'
 
-myframe.plot(title='학생별 누적 시험 성적 점수', kind='bar', rot=0, stacked = True, legend=True)
-filename = 'Ex_p239_07_Graph.png'
+myframe.plot(title='학생별 누적 시험 성적 점수', kind='bar', rot=0, stacked=True, legend=True)
+filename = 'Ex_p239_07.png'
 plt.savefig(filename, dpi=400, bbox_inches='tight')
-plt.savefig(filename, 'saved')
+print(filename + ' saved')
 plt.show()
--- python/pythonData/Ex_p239_08.py
+++ python/pythonData/Ex_p239_08.py
@@ -3,36 +3,27 @@ import matplotlib.pyplot as plt
 
 plt.rcParams['font.family'] = 'NanumBarunGothic'
 
-filename ='tips.csv'
+filename = 'tips.csv'
 
-myframe = pd.read_csv(filename, encoding='utf-8')
-print('head() 메소드 결과')
-print(myframe.head())
-print('-' * 50)
+myframe = pd.read_csv(filename, encoding='utf-8', index_col=0)
 
-print('info() 메소드 결과')
-print(myframe.info())
-print('-' * 50)
-
-mycolor = ['r','b']
-labels = ['Female', 'Male']
-
-
-cnt = 0
+mycolors = ['r', 'b']
+labels = myframe['sex'].unique()
+cnt = 0  # 카운터 변수
 
 for finditem in labels:
     xdata = myframe.loc[myframe['sex'] == finditem, 'total_bill']
     ydata = myframe.loc[myframe['sex'] == finditem, 'tip']
-    plt.plot(xdata, ydata, label=finditem, color=mycolor[cnt], marker='o', linestyle='None')
+    plt.plot(xdata, ydata, color=mycolors[cnt], marker='o', linestyle='None', label=finditem)
     cnt += 1
 
-plt.legend(loc=4)
-plt.xlabel('결제 총액')
-plt.ylabel('팁 비용')
-plt.title('결제 총액과 팁 비용의 산점도')
+plt.legend()
+plt.xlabel("결제 총액")
+plt.ylabel("팁 비용")
+plt.title("결제 총액과 팁 비용의 산점도")
 plt.grid(True)
 
-filename = 'Ex_p239_08_Graph.png'
-plt.savefig(filename, dpi=400, bbox_inches='tight')
+filename = 'Ex_p239_08.png'
+plt.savefig (filename, dpi=400, bbox_inches='tight')
 print(filename + ' saved')
 plt.show()
